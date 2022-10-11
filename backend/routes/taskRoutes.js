602b02a970de09b2c9cca9e57b3b85db884eed76
david.fox <david.fox@albeado.com>
2022-10-11 12:30:56 -0700

    updated ttask routes in backend

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -10,34 +10,33 @@ const dbo = require("../db/conn");
 const ObjectId = require("mongodb").ObjectId;
 
 
-const setTask = (req, res) => {
     // This section will help you create a new record.
-recordRoutes.route("/record/add").post(function (req, response) {
+recordRoutes.route("/tasks/add").post(function (req, response) {
     let db_connect = dbo.getDb();
     let myobj = {
         name: req.body.name,
         position: req.body.position,
         level: req.body.level,
     };
-    db_connect.collection("records").insertOne(myobj, function (err, res) {
+    db_connect.collection("tasks").insertOne(myobj, function (err, res) {
         if (err) throw err;
         response.json(res);
         });
-    });
-}
+});
+
 
 
 // This section will help you get a single record by id
-recordRoutes.route("/record/:id").get(function (req, res) {
+recordRoutes.route("/tasks/:id").get(function (req, res) {
     let db_connect = dbo.getDb();
     let myquery = { _id: ObjectId( req.params.id )};
     db_connect
-        .collection("records")
+        .collection("tasks")
         .findOne(myquery, function (err, result) {
           if (err) throw err;
           res.json(result);
         });
-  });
+});
 
 
 module.exports = recordRoutes;
