f0df096cacf569375392483b7dee12e7bcaff137
david.fox <david.fox@albeado.com>
2022-10-10 20:21:55 -0700

    updated nginx.conf

--- frontend/build-and-push.sh
+++ frontend/build-and-push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
-docker login --username=xxxxx -p xxxxx
+docker login --username=foxy7887 -p MGSGiw550!
 
 image_name=foxy7887/task-tracker-app
-image_tag=v2
+image_tag=v26
 full_image_name=${image_name}:${image_tag}
 
 cd "$(dirname "$0")" 
